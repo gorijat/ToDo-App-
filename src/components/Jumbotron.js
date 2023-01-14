7f6705a5734698c24e437cf4af885c070944779f
Goran Gajic <you@example.com>
2023-01-14 17:18:17 +0100

    create note in react

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const jumbotron = ()=>{
+    return(
+        <div className="jumbotron text-center">
+             <h2>Todo App</h2>
+        </div>
+    )
+}
+
+export default jumbotron;
